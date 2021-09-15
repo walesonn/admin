951bef5e99961e98629e855d9b4171899a1bedfb
faveni <suportedeti17@grupofaveni.com.br>
2021-09-15 17:43:26 -0300

    ..

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+require("dotenv/config");
 const app = express();
 app.use(express.json({ type: "application/json" }));
 app.set("view engine", "ejs");
@@ -6,7 +7,7 @@ app.set("views", __dirname + "/src/view");
 require("./src/routes")(app);
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
-const PORT = process.env.PORT || 80
+const PORT = process.env.PORT || 80;
 server.listen(PORT);
 
 io.on("connect", (socket) => {
